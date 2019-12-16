756b1bf3d25964fe7165082086312e3905aa05e4
Lolo De Rondo <louvefaya@gmail.com>
2019-12-17 00:07:00 +0100

    adding textfield to my screen and bindings properties

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,11 +1,16 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.beans.binding.Bindings;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -13,6 +18,17 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+
+        VBox root = new VBox(10);
+        root.setPadding(new Insets(25));
+        root.setAlignment(Pos.CENTER);
+
+        TextField textField1 = new TextField("Je suis le premier");
+        TextField textField2 = new TextField("Je suis le deuxième");
+
+        //textField1.textProperty().bindBidirectional(textField2.textProperty());
+        Bindings.bindBidirectional(textField1.textProperty(), textField2.textProperty());
+
         //Create a layout and add button to it
         Button dislikeButton = new Button("Bien le bonjour");
 
@@ -23,7 +39,7 @@ public class Main extends Application {
                 System.out.println("Yes i am Happy");
             }
         });
-        
+
         //Or with a  lambda expression
         dislikeButton.setOnAction(e -> System.out.println("Je suis la amène toi"));
 
@@ -31,8 +47,7 @@ public class Main extends Application {
         Text text = new Text();
         text.setText("Je suis le nouveau texte");
 
-        StackPane root = new StackPane();
-        root.getChildren().add(dislikeButton);
+        root.getChildren().addAll(textField1, textField2, dislikeButton);
 
         //create a scene
         Scene scene = new Scene(root, 400, 400);
